7d46ff10fd36bf658122c3d0a98e4ad61cd78f41
Ashkan <AshkanGanj@gmail.com>
2021-08-10 15:51:34 +0430

    get list of pathes for each miss tiles

--- node.py
+++ node.py
@@ -1,4 +1,6 @@
 import numpy as np
+from path import PathFinder
+from itertools import islice
 
 
 class Movements:
@@ -213,7 +215,7 @@ class Heuristic:
         return distance
 
     def missplaced_tiles(self, distance, missplaced_dict=False):
-        tiles_dict = {'list':[], 'index':[]}
+        tiles_dict = {'list': [], 'index': []}
         for i in range(len(self.arr)):
             if self.arr[i] != self.goal[i]:
                 distance = distance + 1
@@ -223,19 +225,45 @@ class Heuristic:
         if missplaced_dict:
             return tiles_dict
         return distance - 1
-    def find_path(self,tile,goal):
-        path = []
-        for i in self.arr:
-            if tile.index == goal:
-                path.append()
-        
+
+    def listToMatrix(self, arr: list, size: int):
+        return [arr[x:x+size] for x in range(0, len(arr), size)]
+
+    def findTile(self, element, matrix):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == element:
+                    return (i, j)
+    # find path for given tile
+    def findPaths(self, start, end, matrix):
+        x, y = start[0], start[1]
+        pathfinder = PathFinder(start, end, matrix)
+        pathfinder.findPath(x, y)
+        paths = pathfinder.paths
+        return paths
+    # get paths for all missplaced tiles
+    def getPaths(self, distance):
+        paths = {}
+        matrix_start = self.listToMatrix(self.arr, 3)
+        matrix_goal = self.listToMatrix(self.goal, 3)
+
+        missplaced_tiles_dict = self.missplaced_tiles(
+            distance, missplaced_dict=True)
+        for tile in missplaced_tiles_dict['list']:
+            index_start = self.findTile(tile, matrix_start)
+            index_end = self.findTile(tile, matrix_goal)
+            paths[str(tile)] = self.findPaths(
+                index_start, index_end, matrix_start)
+        return paths
+
     def new_method(self, distance):
-        distance = self.missplaced_tiles(distance)
-        missplaced_tiles_dict = self.missplaced_tiles(distance, missplaced_dict=True)
-        print(missplaced_tiles_dict)
+        paths = self.getPaths(distance)
+        print("")
         return distance
 
 # Distance Class to Calculate the Manhattan and Misplaced Tiles and new Distance.
+
+
 class Distance:
     def calculate(arr, goal, heuristic):
         distance = 0
--- path.py
+++ path.py
@@ -1,4 +1,4 @@
-import os,time
+import os
 
 class PathFinder:
     def __init__(self, start, end, matrix):
@@ -6,12 +6,14 @@ class PathFinder:
         self.end = end
         self.matrix = matrix
         self.path = []
+        self.paths = []
 
-    def findPaths(self, i, j):
+    def findPath(self, i, j):
+        
         (M, N) = (len(self.matrix), len(self.matrix[0]))
 
         if self.matrix[i][j] == self.matrix[self.end[0]][self.end[1]]:
-            print(self.path + [self.matrix[i][j]])
+            self.paths.append(self.path + [self.matrix[i][j]])
             return
         # # if the last cell is reached, print the route
         # if i == M - 1 and j == N - 1:
@@ -24,54 +26,50 @@ class PathFinder:
         if self.start[0] > self.end[0] and self.start[1] > self.end[1]:
             # move left
             if 0 <= i < M and 0 <= j - 1 < N:
-                self.findPaths(i, j - 1)
+                self.findPath(i, j - 1)
             # move up
             if 0 <= i - 1 < M and 0 <= j < N:
-                self.findPaths(i - 1, j)
+                self.findPath(i - 1, j)
         elif self.start[0] < self.end[0] and self.start[1] < self.end[1]:
             if 0 <= i < M and 0 <= j + 1 < N:
-                self.findPaths(i, j + 1)
+                self.findPath(i, j + 1)
             # move down
             if 0 <= i + 1 < M and 0 <= j < N:
-                self.findPaths(i + 1, j)
+                self.findPath(i + 1, j)
         elif self.start[0] < self.end[0] and self.start[1] > self.end[1]:
             # move left
             if 0 <= i < M and 0 <= j - 1 < N:
-                self.findPaths(i, j - 1)
+                self.findPath(i, j - 1)
 
             # move down
             if 0 <= i + 1 < M and 0 <= j < N:
-                self.findPaths(i + 1, j)
+                self.findPath(i + 1, j)
         elif self.start[0] > self.end[0] and self.start[1] < self.end[1]:
             # move left
             if 0 <= i < M and 0 <= j + 1 < N:
-                self.findPaths(i, j + 1)
+                self.findPath(i, j + 1)
 
             # move down
             if 0 <= i - 1 < M and 0 <= j < N:
-                self.findPaths(i - 1, j)
+                self.findPath(i - 1, j)
 
         # backtrack: remove the current cell from the path
         self.path.pop()
 
 
-if __name__ == '__main__':
-    os.system('cls')
-    # starting time
-    timer_start = time.time()
-    mat = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
+# if __name__ == '__main__':
+#     os.system('cls')
 
-    start = [0, 1]
-    end = [2, 2]
-    x = start[0]
-    y = start[1]
+#     mat = [
+#         [1, 2, 3],
+#         [4, 5, 6],
+#         [7, 8, 9]
+#     ]
 
-    obj = PathFinder(start, end, mat)
+#     start = [0, 1]
+#     end = [2, 2]
+#     x = start[0]
+#     y = start[1]
 
-    obj.findPaths(x, y)
-    timer_end = time.time()
-    print("Execution time:",timer_end - timer_start)
+#     obj = PathFinder(start, end, mat)
+#     obj.findPath(x, y)
